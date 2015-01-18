40da92d4bea8f9ea072c83d7483d460bf89870eb
e-karma <djbradyla@gmail.com>
2015-01-18 10:59:38 -0800

    first

---
+++ app/pods/friends/route.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    save: function() {
+      console.log('+- save action bubbled up to friends route');
+
+      return false;
+    },
+    cancel: function() {
+      console.log('+- cancel action bubbled up to friends route');
+
+      return false;
+    }, 
+    delete: function(friend) {
+      friend.destroyRecord();
+      this.transitionTo('friends.index');
+    }
+  }
+});
